42a5ff2430eb33449140a0f1f868f965ab9e7a86
Mishkin Faustini <mishkinf@hotmail.com>
2016-02-10 01:08:13 -0800

    updating code

--- index.js
+++ index.js
@@ -4,31 +4,26 @@ const { RestApiStoreAdapter } = require('./src/rest-api-store-adapter');
 exports.LocalStoreAdapter = LocalStoreAdapter;
 exports.RestApiStoreAdapter = RestApiStoreAdapter;
 
-
-// store.create = function(noun, item) {
-//     store.adapter.create(item);
-// };
-
-// store.update = function(noun, item) {
-//     store.adapter.update(item);
-// };
-
-// store.read_all = function(noun) {
-//     store.adapter.read_all();    
-// };
-
-// store.delete = function(noun, id) {
-//     store.adapter.delete(id);
-// };
-
-// store.read = function(noun, id) {
-//     store.adapter.read(id);
-// };
-
 exports.registerStoreAdapter = function(store, adapter) {
     store.adapter = adapter;
-};
+    
+    store.create = function(noun, item) {
+        store.adapter.create(item);
+    };
 
-exports.registerStoreAdapter = function(store, adapter) {
-    store.adapter = adapter;
+    store.update = function(noun, item) {
+        store.adapter.update(item);
+    };
+
+    store.read_all = function(noun) {
+        store.adapter.read_all();    
+    };
+
+    store.delete = function(noun, id) {
+        store.adapter.delete(id);
+    };
+
+    store.read = function(noun, id) {
+        store.adapter.read(id);
+    };
 };
